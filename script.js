a0f236665235f26ee6d9f0e2f6e2f319312957de
sebas6292 <64964750+sebas6292@users.noreply.github.com>
2022-09-30 15:13:46 -0700

    2 files modified

--- script.js
+++ script.js
@@ -12,15 +12,19 @@ const UserCard = ({ picture, name, email, phone, dob, ...rest }) => {
   const p3 = document.createElement('button');
   p3.className = 'getdob';
 
+  // const reset = document.createElement('button');
+  // reset.className = 'reset-';
+
   img.src = picture.large;
   h2.innerHTML = `${name.first} ${name.last}`;
   p.innerHTML = `Email: ${email}`;
   p2.innerHTML = `Phone Number: ${phone}`;
   p3.innerHTML = `Display DOB`;
+  // reset.innerHTML = `Reset`;
 
-  p3.addEventListener('click', displayDob);
+  p3.addEventListener('click', toggleDob);
 
-  function displayDob() {
+  function toggleDob() {
     if (p3.innerHTML === 'Display DOB') {
       p3.innerHTML = `Birthday: ${dob.date}`;
     } else {
@@ -28,11 +32,18 @@ const UserCard = ({ picture, name, email, phone, dob, ...rest }) => {
     }
   }
 
+  // card.addEventListener('click', resetAll);
+
+  // function resetAll() {
+  //   card.reset();
+  // }
+
   card.appendChild(img);
   card.appendChild(h2);
   card.appendChild(p);
   card.appendChild(p2);
   card.appendChild(p3);
+  // card.appendChild(reset);
 
   return card;
 };
@@ -52,9 +63,18 @@ const fetchPeople = (event) => {
     });
 };
 
+// const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// const fetchPeople = (id) => async (event) => {
+//   try {
+//     const response = await Promise.all(
+//       ids.map(async id => {
+//           const res = await fetch(URL + id)
+//       })
+//     )
+//   } catch(err) {
+//       debugger;
+//     };
+// };
+
 Btn.addEventListener('click', fetchPeople);
-
-
-function reset(){  
-  document.getElementByClass("container").reset();  
-}   
